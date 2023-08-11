265ba93b618db415ac146f9c2f6eb6f3da25d1fc
cobalt1705 <123169634+cobalt1705@users.noreply.github.com>
2023-08-11 13:08:07 +0300

    Add files via upload

--- stream_dog_cat.py
+++ stream_dog_cat.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import torchvision.models as models
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = torch.load('resnet18_full_model.pth', map_location=device)
+model.eval()
 
 def main():
     # Заголовок приложения
@@ -21,14 +28,37 @@ def show_homepage():
     st.header("Добро пожаловать ")
     # Здесь вы можете добавить любой контент для главной страницы
     # Добавляем картинку
-    st.image("/Users/cobalt/Desktop/blood.png", use_column_width=True)
+    st.image("blood.png", use_column_width=True)
 def show_page1():
     st.header("Классификация ImageNet")
     # Здесь вы можете добавить любой контент для страницы 1
+def predict(image):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    image = transform(image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        output = model(image)
+        predicted = (torch.sigmoid(output) > 0.5).float().item()
+    return "Псина обнаружена" if predicted else "Это котик!"
 
 def show_page2():
+    
     st.header("Классификация моделью ResNet18 Cats&Dogs")
     # Здесь вы можете добавить любой контент для страницы 2
+    
+    uploaded_file = st.file_uploader("Загрузите картинку", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+        label = predict(image)
+        st.write(f'The uploaded image most likely belongs to a {label}.')
 
 def show_page3():
     st.header("Классификация клеток крови")
